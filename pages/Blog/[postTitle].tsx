f3654a25750c39907a620f7977b7e914794891f8
zaccowan <68406370+zaccowan@users.noreply.github.com>
2022-07-08 18:28:42 -0400

    Added Dynamic Routing For Blog Posts
    Just to get font end lined out before implementing backend for post query.
    Queried post data through Link in Blog Card.

---
+++ pages/Blog/[postTitle].tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import router from "next/router";
+
+function PostPage() {
+  const { postImgUrl, postTitle, postParagraph, postDate } = router.query;
+  console.log(router.query);
+  return (
+    <div className="py-40 flex flex-col items-center max-w-4xl mx-auto space-y-8">
+      <div className="flex justify-center items-end space-x-8 w-[100%] px-20">
+        <h1 className="text-6xl font-thin uppercase redToBlueTextGradient">
+          {postTitle}
+        </h1>
+        <p className="text-sm italic">{postDate}</p>
+      </div>
+      <img
+        src={postImgUrl as string}
+        className="rounded-3xl shadow-xl shadow-black/20 border"
+      />
+      <p className="text-center lg:text-left lg:indent-10">{postParagraph}</p>
+    </div>
+  );
+}
+
+export default PostPage;
