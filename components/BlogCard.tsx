95b2f7e33825c378f2f39da214ebb5e458684e9d
zaccowan <68406370+zaccowan@users.noreply.github.com>
2022-07-08 01:28:54 -0400

    Added Some more filler Posts

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -10,13 +10,13 @@ function BlogCard({
     <div className="flex-col lg:flex-row flex space-y-8 lg:space-y-0 justify-between border shadow-lg rounded-2xl lg:hover:scale-110 transition-all duration-300 cursor-pointer group p-8">
       <img
         src={postImgUrl}
-        className="lg:max-w-xl rounded-2xl object-cover border shadow-xl lg:group-hover:scale-95 lg:group-hover:shadow-black/20 transition-all duration-300 ease-out"
+        className="lg:max-w-xl rounded-2xl object-cover my-auto border shadow-xl lg:group-hover:scale-95 lg:group-hover:shadow-black/20 transition-all duration-300 ease-out"
       />
-      <div className="flex flex-col  px-6 space-y-4 text-center lg:text-left">
+      <div className="flex flex-col px-6 space-y-4 text-center lg:text-left">
         <h2 className="font-thin text-4xl tracking-widest redToBlueTextGradient lg:group-hover:translate-x-4 lg:group-hover:font-semibold transition-all duration-500 ease-out">
           {postTitle}
         </h2>
-        <p className="text-lg leading-loose tracking-wide font-serif lg:indent-10 break-all">
+        <p className="text-lg leading-loose tracking-wide font-serif lg:indent-10 break-words">
           {postParagraph}
         </p>
         <p className="italic tracking-widest text-">{postDate}</p>
--- pages/Blog.tsx
+++ pages/Blog.tsx
@@ -5,7 +5,8 @@ function Blog() {
   const testPosts = [
     {
       postId: 1,
-      postImgUrl: "/bg/AboutBG.jpg",
+      postImgUrl:
+        "https://images.unsplash.com/photo-1657114650481-35584a571239?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
       postTitle: "Test Title",
       postParagraph:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis rerum vel voluptatibus nobis dignissimos fugit numquam, error expedita omnis sit! Dolorem ullam, animi maxime tenetur laudantium dolorum ipsa deleniti at. Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi temporibus repudiandae obcaecati nemo dicta veniam asperiores sed autem quis vitae?",
@@ -17,14 +18,33 @@ function Blog() {
         "https://images.unsplash.com/photo-1657120109154-f582a33fc546?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       postTitle: "Unspash Image Test",
       postParagraph:
-        "This post is a test to assure sizing and responsiveness of these blog cards here are in tip top shape. For now, lets just appreciate the photograph by a lovely indivdual on unsplash. Heres the link: https://images.unsplash.com/photo-1657120109154-f582a33fc546?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        "This post is a test to assure sizing and responsiveness of these blog cards here are in tip top shape. For now, lets just appreciate the photograph by a lovely indivdual on unsplash.",
+      postDate: "07/08/2022",
+    },
+    {
+      postId: 3,
+      postImgUrl:
+        "https://images.unsplash.com/photo-1656989048329-4226c441e501?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=652&q=80",
+      postTitle: "Filler Content Test",
+      postParagraph:
+        "Heres a bit of a shorter summary for fit testing. I dont know where this photo was taken but its pretty cool to look at!",
+      postDate: "07/08/2022",
+    },
+    {
+      postId: 4,
+      postImgUrl:
+        "https://images.unsplash.com/photo-1533000971552-6a962ff0b9f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80",
+      postTitle: "Okay Im done.",
+      postParagraph:
+        "Cool picture of a bible at sunset. Maybe sunrise. Who knows? But does it really matter? Nope. Okay Im really done now. Bye.",
       postDate: "07/08/2022",
     },
   ];
   return (
-    <div className="py-20 mx-4 lg:max-w-5xl lg:mx-auto h-screen">
+    <div className="py-20 mx-4 lg:max-w-5xl lg:mx-auto">
       <h1 className="font-semibold text-4xl redToBlueTextGradient py-4">
-        Welcome To My Blog
+        Welcome To My Blog <br />
+        (This Area is Under Construction and Is Not Yet Active)
       </h1>
       <hr className="border-t-2 py-10" />
       <div className="space-y-20">
@@ -39,12 +59,12 @@ function Blog() {
         ))}
       </div>
 
-      <div className="absolute bottom-0 top-0 left-0 right-0 py-80 text-6xl text-center bg-black/20 backdrop-blur-sm">
+      {/* <div className="absolute bottom-0 top-0 left-0 right-0 py-80 text-6xl text-center bg-black/20 backdrop-blur-sm">
         <h1 className="text-white font-bold drop-shadow-2xl">
           Blog Under Construction <br />
           Come back later!
         </h1>
-      </div>
+      </div> */}
     </div>
   );
 }
